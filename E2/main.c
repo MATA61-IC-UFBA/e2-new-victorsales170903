3aabbfff3448534cf3f3536271e34121c8150644
victorsales170903 <victorsales@ufba.br>
2024-08-12 22:17:18 -0300

    Fazendo atividade

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,39 @@
 /* main.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
 
+extern int yylex();
+extern char* yytext;
+
+int main() {
+    int token;
+
+    while ((token = yylex()) != 0) {
+        switch (token) {
+            case PLUS:
+                printf("Código do token: %d\n", token);
+                break;
+            case MINUS:
+                printf("Código do token: %d\n", token);
+                break;
+            case TIMES:
+                printf("Código do token: %d\n", token);
+                break;
+            case DIV:
+                printf("Código do token: %d\n", token);
+                break;
+            case EOL:
+                printf("Código do token: %d\n", token);
+                break;
+            case ERROR:
+                printf("Código do token: %d\n", token);
+                break;
+            case NUMBER:
+                printf("Constante decimal: %s\n", yytext);
+                break;
+        }
+    }
+
+    return 0;
+}
--- E2/token.h
+++ E2/token.h
@@ -1,1 +1,15 @@
 /* token.h */
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum {
+    NUMBER = 4,
+    PLUS = 1,
+    MINUS = 2,
+    TIMES = 3,
+    DIV = 5,
+    EOL = 0,
+    ERROR = 6
+} Token;
+
+#endif
